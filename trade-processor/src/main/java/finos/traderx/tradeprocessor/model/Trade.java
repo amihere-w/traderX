c37199c7cb44fb63f33df9e54d8505df2556fc90
kwaw.amihere <kwaw.amihere@turntabl.io>
2024-10-28 01:53:37 +0000

    integrate morphir with traderx
    include morphir ir temporarily
    
    include model in trade processor
    
    remove npm install
    
    add dependencies for morphir, aspect
    
    include aspect, annotation
    
    include build from morphir elm source
    
    add models/dist to gitignore
    
    refactor trade processor models into models folder
    
    WIP
    
    trade status as annotation fixed
    
    update entity field mapping
    
    add simple thread pause for trade service
    
    combine rules
    
    consolidate morphir rule
    
    remodelled trade process
    
    refactor java code after model updates
    
    convert bix rules error response into simple string
    
    parse error message to return in exception
    
    comments
    
    add jackson scala module
    
    add json converters for morphir model classes
    
    WIP
    
    add morphir tests
    
    add serializers
    
    Revert "add serializers"
    
    This reverts commit 9dbf7fde2245733b35256feb72a3efc90250ad1a.
    
    add json serializer to morphir
    
    add -f and circe to models build
    
    fix models compilation error
    
    fix trade processor docker build
    
    trade rules modified
    
    include action on trade object for trade service
    
    update models hashes
    
    include (de)serializers for trade objects
    
    add scheduling for trade processor
    
    simplify morphir aspect
    
    include new path for processor
    
    include necessary fields for trade feed rows
    
    add uuid
    
    include action,state,id to TradeTicket
    
    include cancel trade option
    
    fetch cancelled order with id
    
    fix formatting
    
    avoid position > 0 when processing
    
    allow trade/pos updates to be flattened into one row based on id
    
    filter trades correctly
    
    title case for side
    
    fix cancellation bugs
    
    apply spread operator
    
    simple format
    
    abstract hibernate converter
    
    update service to use correct trade id for settlement
    
    add print statement
    
    trade rules modified and scala code gitignored
    
    fix new model changes
    
    remove maybe in trade order object
    
    trade rules modified
    
    removed desired action

--- trade-processor/src/main/java/finos/traderx/tradeprocessor/model/Trade.java
+++ trade-processor/src/main/java/finos/traderx/tradeprocessor/model/Trade.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,6 +14,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import traderx.morphir.rulesengine.models.TradeSide.TradeSide;
+import traderx.morphir.rulesengine.models.TradeState.TradeState;
 
 @Entity
 @Table(name = "TRADES")
@@ -20,97 +24,62 @@ public class Trade implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Column(length = 100, name = "ID")
-    @Id
-	private String id;
+  @Column(length = 100, name = "ID")
+  @Id
+  private String id;
 
-	public String getId() {
-		return this.id;
-	}
+  public String getId() { return this.id; }
 
-	public void setId(String id) {
-		this.id = id;
-	}
-    
-	@Column(name = "ACCOUNTID")
-	private Integer accountId;
+  public void setId(String id) { this.id = id; }
 
-	public Integer getAccountId() {
-		return this.accountId;
-	}
+  @Column(name = "ACCOUNTID")
+  private Integer accountId;
 
-	public void setAccountId(Integer id) {
-		this.accountId = id;
-	}
+  public Integer getAccountId() { return this.accountId; }
 
+  public void setAccountId(Integer id) { this.accountId = id; }
 
+  @Column(length = 50, name = "SECURITY")
+  private String security;
 
-    @Column(length = 50, name = "SECURITY")
-	private String security;
+  public String getSecurity() { return this.security; }
 
-	public String getSecurity() {
-		return this.security;
-	}
+  public void setSecurity(String security) { this.security = security; }
 
-	public void setSecurity(String security) {
-		this.security = security;
-	}
+  @Column(name = "SIDE")
+  @Convert(converter = TradeSideConverter.class)
+  private TradeSide side;
 
-	@Enumerated(EnumType.STRING)
-    @Column(length = 4, name = "SIDE")
-	private TradeSide side;
+  public TradeSide getSide() { return this.side; }
 
-	public TradeSide getSide() {
-		return this.side;
-	}
+  public void setSide(TradeSide side) { this.side = side; }
 
-	public void setSide(TradeSide side) {
-		this.side = side;
-	}
+  @Column(name = "STATE")
+  @Convert(converter = TradeStateConverter.class)
+  private TradeState state;
 
-	@Enumerated(EnumType.STRING)
-    @Column(length = 20, name = "STATE")
-	private TradeState state=TradeState.New;
+  public TradeState getState() { return this.state; }
 
-	public TradeState getState() {
-		return this.state;
-	}
+  public void setState(TradeState state) { this.state = state; }
 
-	public void setState(TradeState state) {
-		this.state = state;
-	}
+  @Column(name = "QUANTITY")
+  private Integer quantity;
 
-	@Column(name = "QUANTITY")
-	private Integer quantity;
+  public Integer getQuantity() { return this.quantity; }
 
-	public Integer getQuantity() {
-		return this.quantity;
-	}
+  public void setQuantity(Integer quantity) { this.quantity = quantity; }
 
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
+  @Column(name = "UPDATED")
+  private Date updated;
 
-	@Column(name = "UPDATED")
-	private Date updated;
+  public void setUpdated(Date u) { this.updated = u; }
 
-	public void setUpdated(Date u){
-		this.updated=u;
-	}
+  public Date getUpdated() { return this.updated; }
 
-	public Date getUpdated(){
-		return this.updated;
-	}
+  @Column(name = "CREATED")
+  private Date created;
 
+  public void setCreated(Date u) { this.created = u; }
 
-	@Column(name = "CREATED")
-	private Date created;
-
-	public void setCreated(Date u){
-		this.created=u;
-	}
-
-	public Date getCreated(){
-        return this.created;
-	}
+  public Date getCreated() { return this.created; }
 }
